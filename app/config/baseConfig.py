4d97cedf054ab473fba07fc10692781402036d4a
maple-flowers <120464022+maple-flowers@users.noreply.github.com>
2025-07-19 10:24:44 +0800

    Update baseConfig.py

--- app/config/baseConfig.py
+++ app/config/baseConfig.py
@@ -6,7 +6,7 @@ from app.utils.templateMaker.templateMaker import pushWJWDataToDB
 
 class Config:
     # 创建秘钥
-    SECRET_KEY = 'yzl'
+    SECRET_KEY = 'sgg'
 
     # jsonify配置
     JSON_AS_ASCII = False
@@ -16,23 +16,23 @@ class Config:
 
     # 邮件配置
     MAIL_SERVER = 'smtp.qq.com'
-    MAIL_USERNAME = '1877515277@qq.com'
-    MAIL_PASSWORD = 'jiyihuhfdvqncedi'
+    MAIL_USERNAME = '2917418117@qq.com'
+    MAIL_PASSWORD = '19640422hhy'
     MAIL_PORT = 465
-    MAIL_DEFAULT_SENDER = '1877515277@qq.com'
+    MAIL_DEFAULT_SENDER = '2917418117@qq.com'
     MAIL_USE_SSL = True
     MAIL_USE_TLS = False
 
     # 管理员邮箱(自行设定，用来发送一些任务完成信息 例如爬虫任务)
-    ADMIN_EMAIL = "yuzl1123@163.com"
+    ADMIN_EMAIL = "2917418117@qq.com"
 
     # 模板管理用户的用户名(该用户下的所有问卷都是模板 便于管理 如果不存在会自动创建一个)
     TEMPALTES_MANAGER = "TEMPLATE_MAKER"
 
     # 微信小程序相关
     # APP_ID
-    MINI_PROGRAM_APPID = "这里填写微信小程序appid"
-    MINI_PROGRAM_APPSECRET = "这里填写小程序appsecret"
+    MINI_PROGRAM_APPID = "wx91021308f65cc4df"
+    MINI_PROGRAM_APPSECRET = "336829b613260e34491a6ac952bec58d"
 
 
 class DevelopmentConfig(Config):
@@ -43,7 +43,7 @@ class DevelopmentConfig(Config):
     }
 
     # 开发环境下web端的url
-    WEB_BASE_URL = "http://192.168.0.129:8080"
+    WEB_BASE_URL = "http://127.0.0.1:80"
 
 
 class ProductionConfig(Config):
